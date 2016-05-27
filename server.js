2f561591c2c5315e7be0442599ba3681c3f3e13b
BasSondag <bassondag@hotmail.com>
2016-05-27 10:26:54 -0700

    delet user[socketID] in chat room is working

--- server.js
+++ server.js
@@ -25,15 +25,16 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({extended: true})); // get information from html forms
+app.use(express.static(path.join(__dirname,'./client')));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ secret: 'thisgameisverrysecret' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-app.set('views', path.join(__dirname, "./views"));
+app.set('views', path.join(__dirname, "./client/views"));
 
 // routes ======================================================================
 
@@ -94,6 +95,16 @@ io.sockets.on('connection', function (socket) {
 		messages.push({name: data.user, message: data.message});
 		console.log(messages);
 		io.emit('messages', messages)
+	})
+
+	socket.on('disconnect', function(){
+		console.log('logging out')
+		// Create a message that you will send to all other users, letting them know 
+		// that this user has logged out of the chat
+		message = users[socketID] + ' has just logged out';
+		// delete that user from the list of users 
+		delete users[socketID];
+		socket.broadcast.emit('logout', {message: message});
 	})
 
 
